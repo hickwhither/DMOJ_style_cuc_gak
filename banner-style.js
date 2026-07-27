4b143b26ded0587a7b030d999ba9fe5dba1f2f3f
hickwhither <92677288+hickwhither@users.noreply.github.com>
2026-07-27 12:30:10 +0700

    hehe

--- banner-style.js
+++ banner-style.js
@@ -4,6 +4,10 @@ window.initBannerStyle = function () {
             'banner-Spiders-Ryoshu',
             'https://media.tenor.com/aURtRU34d5EAAAAj/valencina-house-of-spiders.gif'
         ],
+        [
+            'Hololive',
+            'https://5987e038-eec4-453b-939f-2c0c63b0e9d6.cktoj.com/martor/0d5483dc-391c-46a4-bb8c-ac35fca7e091.png'
+        ]
         // Thêm banner khác ở đây nếu cần
     ]);
 
